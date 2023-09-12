58137c366583c309ece950a29bed53c5e6b5184f
Saleh <salehshareef88@gmail.com>
2023-09-12 16:22:23 +0300

    ok

--- src/test/java/Home/HeaderSection.java
+++ src/test/java/Home/HeaderSection.java
@@ -6,9 +6,15 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 import static java.lang.Thread.sleep;
 
@@ -22,7 +28,10 @@ public class HeaderSection {
     WebElement firstHeaderPhoto;
     WebElement firstButtonHeader;
     WebElement appStorePageTitle;
-    WebElement officialAgencySection;
+    WebElement officialAgencyText;
+    WebElement officialAgencyPhoto;
+    WebElement officialAgencyTitle;
+    Wait<WebDriver> wait;
     JavascriptExecutor jse ;
 
 
@@ -33,13 +42,14 @@ public class HeaderSection {
         options=new ChromeOptions();
         options.addArguments("start-maximized");
         driver=WebDriverManager.chromedriver().capabilities(options).create();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        wait=new WebDriverWait(driver,Duration.ofSeconds(5));
         driver.get("https://famcare.app");
         jse= (JavascriptExecutor) driver;
     }
 
 @Test(priority = 1)
-public void HeaderTextisDisplayed() throws InterruptedException {
-    sleep(5000);
+public void HeaderTextisDisplayed()  {
    HeaderText =driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[1]/h2"));
     Assert.assertTrue(HeaderText.isDisplayed());
 }
@@ -70,30 +80,40 @@ public void HeaderTextisCorrect(){
         firstButtonHeader.click();
 }
 @Test(priority = 7)
-    public void FirstbuttonNavigate() throws InterruptedException {
-        sleep(5000);
+    public void FirstbuttonNavigate()  {
         appStorePageTitle=driver.findElement(By.xpath("/html/body/div[3]/main/div[2]/section[1]/div/div[2]/header/h1"));
         Assert.assertEquals(appStorePageTitle.getText(),"فامكير- جلسات نفسية واجتماعية 4+");
         driver.navigate().back();
-        sleep(5000);
 
     }
     @Test(priority = 8)
-    public void OfficialAgenciesVerify() throws InterruptedException {
+    public void OfficialAgenceyTitleVerify() {
         //Check the Title
-        officialAgencySection=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[1]/h2"));
-        Assert.assertTrue(officialAgencySection.isDisplayed());
-        Assert.assertEquals(officialAgencySection.getText(),"تطبيق معتمد من الجهات الرسمية");
-       //Check photo
-        officialAgencySection=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[2]/div[4]/div/div[1]/img"));
-        Assert.assertTrue(officialAgencySection.isDisplayed());
-        //Check Text
         jse.executeScript("window.scrollBy(0,250)");
-        sleep(2000);
-        officialAgencySection=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[2]/div[4]/div/div[2]/h3"));
-        Assert.assertTrue(officialAgencySection.isDisplayed());
-        Assert.assertEquals(officialAgencySection.getText(),"مركز طب اتصالي");
+        officialAgencyTitle = driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[1]/h2"));
+        Assert.assertTrue(officialAgencyTitle.isDisplayed());
+        Assert.assertEquals(officialAgencyTitle.getText(), "تطبيق معتمد من الجهات الرسمية");
     }
+
+        //Check photo
+        @Test(priority = 9)
+        public void OfficialAgenceyPhotoVerify() {
+            jse.executeScript("window.scrollBy(0,250)");
+            officialAgencyPhoto = driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[2]/div[4]/div/div[1]/img"));
+            Assert.assertTrue(officialAgencyPhoto.isDisplayed());
+        }
+
+
+        //Check Text
+        @Test(priority = 10)
+        public void OfficialAgencyTextverify(){
+         jse.executeScript("window.scrollBy(0,300)");
+         officialAgencyText=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[2]/div[4]/div/div[2]/h3"));
+        Assert.assertTrue(officialAgencyText.isDisplayed());
+        Assert.assertEquals(officialAgencyText.getText(),"مركز طب اتصالي");
+    }
+
+
 
 
 }
