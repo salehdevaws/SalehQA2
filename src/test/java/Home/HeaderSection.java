1b767a5675ec04d25a34e7e8f2acbc044282aa64
sss <salehshareef88@gmail.com>
2023-09-12 15:29:05 +0300

    ok

--- src/test/java/Home/HeaderSection.java
+++ src/test/java/Home/HeaderSection.java
@@ -22,6 +22,7 @@ public class HeaderSection {
     WebElement firstHeaderPhoto;
     WebElement firstButtonHeader;
     WebElement appStorePageTitle;
+    WebElement officialAgencySection;
     JavascriptExecutor jse ;
 
 
@@ -50,34 +51,48 @@ public void HeaderTextisCorrect(){
     public void SecondTextisDisplayed(){
     SecondText =driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[2]"));
     Assert.assertTrue(SecondText.isDisplayed());
+  Assert.assertEquals(SecondText.getText(),"لاتستهين بخطواتك، أخصائيين مرخصين متوفرين بتخصصات مختلفة .. خذ القرار الآن ");
 }
-@Test (priority = 4)
-public void SecondTextisCorrect(){
-    Assert.assertEquals(SecondText.getText(),"لاتستهين بخطواتك، أخصائيين مرخصين متوفرين بتخصصات مختلفة .. خذ القرار الآن ");
-}
-@Test(priority = 5)
+
+@Test(priority = 4)
     public void PhotoisDisplayed(){
     firstHeaderPhoto =driver.findElement(By.xpath("//*[@id=\"heroSection\"]/div/div/div[1]/div/div[1]/img"));
 }
-@Test(priority = 6)
+@Test(priority = 5)
     public void FisrtButtonisDisplayed(){
-firstButtonHeader = driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
-}
-@Test(priority = 7)
-    public void FirstButtonTextisCorrect(){
-        Assert.assertEquals(firstButtonHeader.getText(),"احجز جلستك الأولى");
-}
-@Test(priority = 8)
+    firstButtonHeader = driver.findElement(By.xpath("//*[@id=\"heroText\"]/div/div[3]/a"));
+    Assert.assertEquals(firstButtonHeader.getText(),"احجز جلستك الأولى");
+    }
+
+@Test(priority = 6)
     public void FirstButtonClicked(){
+
         firstButtonHeader.click();
 }
-@Test(priority = 9)
+@Test(priority = 7)
     public void FirstbuttonNavigate() throws InterruptedException {
         sleep(5000);
         appStorePageTitle=driver.findElement(By.xpath("/html/body/div[3]/main/div[2]/section[1]/div/div[2]/header/h1"));
         Assert.assertEquals(appStorePageTitle.getText(),"فامكير- جلسات نفسية واجتماعية 4+");
+        driver.navigate().back();
+        sleep(5000);
 
-
+    }
+    @Test(priority = 8)
+    public void OfficialAgenciesVerify() throws InterruptedException {
+        //Check the Title
+        officialAgencySection=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[1]/h2"));
+        Assert.assertTrue(officialAgencySection.isDisplayed());
+        Assert.assertEquals(officialAgencySection.getText(),"تطبيق معتمد من الجهات الرسمية");
+       //Check photo
+        officialAgencySection=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[2]/div[4]/div/div[1]/img"));
+        Assert.assertTrue(officialAgencySection.isDisplayed());
+        //Check Text
+        jse.executeScript("window.scrollBy(0,250)");
+        sleep(2000);
+        officialAgencySection=driver.findElement(By.xpath("/html/body/div[1]/section[2]/div/div/div/div/div[2]/div[4]/div/div[2]/h3"));
+        Assert.assertTrue(officialAgencySection.isDisplayed());
+        Assert.assertEquals(officialAgencySection.getText(),"مركز طب اتصالي");
     }
 
 
